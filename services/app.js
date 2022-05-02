51c4658a8b3e5c566abc53062afb04af20162926
bhanumanojbade <79238052+bhanumanojbade@users.noreply.github.com>
2022-05-02 12:17:38 -0500

    Update app.js

--- services/app.js
+++ services/app.js
@@ -54,5 +54,76 @@ module.exports.login = async (options) => {
     })
 }
 
+module.exports.getAllProducts=async (options)=>{
+    return new Promise(async (resolve, reject) => {
+                options.mongoClient.connect(async (err, client) => {
+                    console.log("Connected successfully to  mdb server");
+                    try {
+                        var db = client.db("manojBade");
+                        db.collection("products").find({}).toArray(function (err, result) {
+                            if (err) throw err
+                            if (result) {
+                                resolve({
+                                    response: result,
+                                    status: 200
+                                })
+                            }
+                        });
+                    }
+                    catch (e) {
+                        reject({
+                            response: e,
+                            status: 400
+                        })
+                    }
+                });
+            })
+        }
 
 
+        module.exports.signUp = async (options) => {
+            return new Promise(async (resolve, reject) => {
+                options.mongoClient.connect(async (err, client) => {
+                    console.log("Connected successfully to  mdb server");
+                    var body = options.body
+                    console.log("body", body)
+                    try {
+                        var db = client.db("manojBade");
+                        var userObj = {
+                            "userName": body.name,
+                            "password": body.password,
+                            "email":body.email,
+                            "orderHistory":[]
+                        };
+                        var query = { userName: body.name };
+                        db.collection("users").find(query).toArray(function (err, result) {
+                            if (err) throw err
+                            if (result.length > 0) {
+                                reject({
+                                    response: "choose another name",
+                                    status: 400
+                                })
+                            } else {
+                                db.collection("users").insertOne(userObj, function (err, res) {
+                                    if (err) throw err;
+                                   
+                                    resolve(result = {
+                                        response: "created succesfully",
+                                        status: 200
+                                    })
+                                    //   client.close();
+                                });
+                            }
+                        });
+        
+        
+                    } catch (error) {
+                        
+                    }
+                    
+                })
+        
+             }
+            )
+        }
+        
